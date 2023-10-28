14cc84c935e9c46079bd625cd285da817a045146
SanielCode <saniel.infante@hotmail.com>
2023-10-27 23:48:38 -0400

    First commit

---
+++ Leet-code-return-length.js
@@ -0,0 +1,12 @@
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+// JS CODE HERE
+
+function argumentsLength() {
+    return arguments.length;
+  }
+  
+  
+  console.log(argumentsLength(1, 2, 3));
+  console.log(argumentsLength("apple", "banana")); 
+  console.log(argumentsLength());
